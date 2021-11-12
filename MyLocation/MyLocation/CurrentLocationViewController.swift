f9bfd77ace372a47572e81fde7ee8db6e8acebdb
Stepan Kirillov <stepakirillov59@gmail.com>
2021-11-12 20:10:11 +0300

    Fresh start

---
+++ MyLocation/MyLocation/CurrentLocationViewController.swift
@@ -0,0 +1,13 @@
+//
+//  CurrentLocationViewController.swift
+//  MyLocation
+//
+//  Created by Stepan Kirillov on 11/12/21.
+//
+
+import Foundation
+import UIKit
+
+class CurrentLocationViewController: UIViewController {
+	
+}
---
+++ MyLocation/MyLocation/TabBarController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyLocation
+//
+//  Created by Stepan Kirillov on 11/12/21.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
