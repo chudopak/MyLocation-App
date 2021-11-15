6ed45fa233a11038471df1b0227a151806d4c971
Stepan Kirillov <stepakirillov59@gmail.com>
2021-11-15 14:15:46 +0300

    Able to get device location

--- MyLocation/MyLocation/CurrentLocationViewController.swift
+++ MyLocation/MyLocation/CurrentLocationViewController.swift
@@ -7,7 +7,62 @@
 
 import Foundation
 import UIKit
+import CoreLocation
 
-class CurrentLocationViewController: UIViewController {
+class CurrentLocationViewController: UIViewController, CLLocationManagerDelegate {
+	
+	let locationManager = CLLocationManager()
+	
+	@IBOutlet weak var messageLabel: UILabel!
+	@IBOutlet weak var latitudeLabel: UILabel!
+	@IBOutlet weak var longitudeLabel: UILabel!
+	@IBOutlet weak var addressLabel: UILabel!
+	@IBOutlet weak var tagButton: UIButton!
+	@IBOutlet weak var getButton: UIButton!
+	
+	@IBAction func getLocation() {
+		let authorizationStatus = locationManager.authorizationStatus
+		
+		switch authorizationStatus {
+		case .notDetermined:
+			locationManager.requestWhenInUseAuthorization()
+			return
+		case .denied, .restricted:
+			showLocationServicesDeniedAlert()
+			return
+		default:
+			print("All ok")
+		}
+		
+		if authorizationStatus == .notDetermined {
+			locationManager.requestWhenInUseAuthorization()
+			return
+		}
+		
+		locationManager.delegate = self
+		locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+		locationManager.startUpdatingLocation()
+	}
+	
+	func showLocationServicesDeniedAlert() {
+		let alert = UIAlertController(title: "Location Disabeled",
+									  message: "Go to preferences and enable location ",
+									  preferredStyle: .alert)
+		let okAlert = UIAlertAction(title: "OK",
+									style: .default,
+									handler: nil)
+		alert.addAction(okAlert)
+		present(alert, animated: true, completion: nil)
+	}
+	
+	//MARK: - CLLocationManagerDelegate
+	func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+		print("didFailWitherror \(error)")
+	}
+	
+	func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+		let newLocation = locations.last!
+		print("didUpdateLocations \(newLocation)")
+	}
 	
 }
